0c9b95719cc1e5ab5f5ca3506ec5f906a98df159
Jonathan Czepa <jonathan.czepa@gmail.com>
2024-04-21 16:48:36 -0500

    Properly privatized instance variables. This issue was first noticed and fixed in the test branch.

--- television.py
+++ television.py
@@ -1,55 +1,53 @@
 
 class Television():
 
-    #test to see if pushing works
-
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
     def __init__(self):
-        self._status = False
-        self._muted = False
-        self._volume = Television.MIN_VOLUME
-        self._channel = Television.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        self._status ^= True
+        self.__status ^= True
             
     def mute(self):
-        if self._status:
-            self._muted ^= True
+        if self.__status:
+            self.__muted ^= True
 
     def channel_up(self):
-        if self._status:
-            if self._channel < Television.MAX_CHANNEL: 
-                self._channel += 1
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL: 
+                self.__channel += 1
             else:
-                self._channel = Television.MIN_CHANNEL
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
-        if self._status:
-            if self._channel > Television.MIN_CHANNEL: 
-                self._channel -= 1
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL: 
+                self.__channel -= 1
             else:
-                self._channel = Television.MAX_CHANNEL
+                self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
-        if self._status:
-            self._muted = False
-            if self._volume < Television.MAX_VOLUME: 
-                self._volume += 1
+        if self.__status:
+            self.__muted = False
+            if self.__volume < Television.MAX_VOLUME: 
+                self.__volume += 1
             
 
     def volume_down(self):
-        if self._status:
-            self._muted = False
-            if self._volume > Television.MIN_VOLUME: 
-                self._volume -= 1
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME: 
+                self.__volume -= 1
 
     def __str__(self):
-        if self._muted == True:
+        if self.__muted == True:
             return f"Power = {self._status}, Channel = {self._channel}, Volume = {Television.MIN_VOLUME}"
         else:
             return f"Power = {self._status}, Channel = {self._channel}, Volume = {self._volume}"
